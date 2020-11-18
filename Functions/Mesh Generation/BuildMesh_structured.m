8f6e5e2d510e0449c50894ea6f6a2b5c5e550d53
Saeed <shatefia@uwaterloo.ca>
2020-11-18 14:29:24 -0500

    Edit function header

--- Functions/Mesh Generation/BuildMesh_structured.m
+++ Functions/Mesh Generation/BuildMesh_structured.m
@@ -12,7 +12,7 @@ function Mesh = BuildMesh_structured(nsd, x1, L, nex, type)
 %   x1:     Starting location for the rectangular mesh (size nsd x 1)
 %   L:      Length of domain (size nsd x 1)
 %   nex:    Number of elements in each direction (size nsd x 1)
-%   type:   the toplogical class of finite element; it is in the general
+%   type:   the topological class of finite element; it is in the general
 %           form 'topology-#of nodes' ie a three node triangle is T3 a 
 %           four node quadralateral is Q4 a 4 node tetrahedra is H4 a 27 
 %           node brick is B27 etc. Presently defined are L2, Q4, and Q9.  
