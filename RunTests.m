f19e14817af123be1777aa455e22fdeeb6892305
Bruce <71358608+b3gee@users.noreply.github.com>
2020-12-11 12:03:40 -0500

    Rename RunTest files

--- RunTests.m
+++ RunTests.m
@@ -35,25 +35,25 @@
 
 %% Test 1: Patch Test A - All nodal displacements prescribed
 % Pass Condition: FEA solution displacements, stresses, and strains are exact
-        run('Test Files/RunTest1')
+        run('Test Files/RunT1_PatchTestA')
 
 %% Test 2: Patch Test B - Dirichlet-Dirichlet BC
 % Pass Condition: FEA solution displacements, stresses, and strains are exact
-        run('Test Files/RunTest2')
+        run('Test Files/RunT2_PatchTestB')
 
 %% Test 3: Patch Test C - Dirichlet-Neumann BC
 % Pass Condition: FEA solution displacements, stresses, and strains are exact
-        run('Test Files/RunTest3')
+        run('Test Files/RunT3_PatchTestC')
 
 %% Test 4: Manufactured Solution - Q4 element convergence
 %   Pass condition: L2-norm converges at a rate of at least h^2
 %                    e-norm converges at a rate of at least h
-         run('Test Files/RunTest4')
+         run('Test Files/RunT4_ManSolQ4')
  
 %% Test 5: Manufactured Solution - Q9 element convergence
 %   Pass condition: L2-norm converges at a rate of at least h^3
 %                    e-norm converges at a rate of at least h^2
-        run('Test Files/RunTest5')
+        run('Test Files/RunT5_ManSolQ9')
         
 %% Test X: [Test Name] - Short Test Description
 %   Pass Condtion:
