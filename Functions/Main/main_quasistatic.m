e2fab9a58b2df85dd5e91de82e4f9869f79eb6b0
Bruce <71358608+b3gee@users.noreply.github.com>
2023-01-24 12:31:13 -0500

    Reconfigure main file for timestep loop

--- Functions/Main/main_quasistatic.m
+++ Functions/Main/main_quasistatic.m
@@ -1,4 +1,4 @@
-% Static solid deformation of linear elastic material
+% Quasi-Static solid deformation of linear elastic material
 
 % Acknowledgements: Matin Parchei Esfahani
 
@@ -16,8 +16,14 @@
     if progress_on
         fprintf('%.2f: Reading config file...\n', toc);
     end
-    [Mesh, Material, BC, Control] = ...
+    [Mesh, Material, BC, Control, IC] = ...
             feval(config_name, ConfigDir, progress_on);
+        
+%% Initialize time variables
+    t = Control.StartTime;
+    dt = Control.TimeStep;
+    t = t + dt;
+    step_count = 0;
 
 %% Set Default Values
     [Mesh, Material, BC, Control] = setDefaults(Mesh, Material, BC, Control);
@@ -46,44 +52,76 @@
     end
     K = getK(Mesh, Quad, Material);
     
+
+%% Define initial conditions
+    
+    d0 = IC.d0;
+    d0(BC.fix_disp_dof) = BC.fix_disp_value;
+    Fext = getFext(Mesh, BC, Quad);
+    
+    % Export initial conditions
+        % Strain
+        [strain, stress] = getStrain(d0, Mesh, Material, Control.stress_calc, Quad);   
+
+    % Force vectors
+        Fint = K*d0;
+
+    % Write to vtk
+        if plot2vtk
+            write2vtk_quasistatic(config_name, vtk_dir, Mesh, Control, BC.fixed, d0, strain, stress, ...
+                            Fint, Fext, step_count);
+        end
+        step_count = step_count + 1;
+        
+ %% Solve the time-dependent problem
+ while t <= Control.EndTime
+    if progress_on
+        fprintf(['\n', num2str(toc),': Computing Time = %.2f s, Timestep %d, Progress %.2f percent...\n'], t, step_count, t/Control.EndTime*100);
+    end
     % Compute external force vector
     if progress_on
         disp([num2str(toc),': Compute Force Vector...']);
     end
     Fext = getFext(Mesh, BC, Quad);
+    % Todo: Make the boundary conditions time dependent functions
+     
+     % Solve linear system of equations
+        if progress_on
+            disp([num2str(toc),': Solve System of Equations...']);
+        end
+        switch Control.LinearSolver
+            case 'LinearSolver1'
+                [d, f_fixed] = LinearSolver1(K, Fext, BC.fix_disp_value, ...
+                                                BC.free, BC.fixed, Control.parallel);
+            case 'LinearSolver2'
+                [d, f_fixed] = LinearSolver2(K,Fext,BC.fix_disp_value, ...
+                                                BC.free, BC.fixed, Control.parallel);
+            case 'LinearSolver3'
+                [d, f_fixed] = LinearSolver3(K, Fext, BC.fix_disp_value, ...
+                                                BC.free, BC.fixed, Control.beta, Control.parallel);
+        end
+        Fext(BC.fixed) = f_fixed;
 
-%% Define initial conditions
-    % For static solution, always assume that free nodes are initially at
-    % zero (undisplaced)
-    d0 = zeros(Mesh.nDOF, 1);
-    d0(BC.fix_disp_dof) = BC.fix_disp_value;
+    % Strain
+        if progress_on
+            disp([num2str(toc),': Post-Processing...']);
+        end
+        [strain, stress] = getStrain(d, Mesh, Material, Control.stress_calc, Quad);   
 
-%% Solve linear system of equations
-    switch Control.LinearSolver
-        case 'LinearSolver1'
-            [d, f_fixed] = LinearSolver1(K, Fext, BC.fix_disp_value, ...
-                                            BC.free, BC.fixed, Control.parallel);
-        case 'LinearSolver2'
-            [d, f_fixed] = LinearSolver2(K,Fext,BC.fix_disp_value, ...
-                                            BC.free, BC.fixed, Control.parallel);
-        case 'LinearSolver3'
-            [d, f_fixed] = LinearSolver3(K, Fext, BC.fix_disp_value, ...
-                                            BC.free, BC.fixed, Control.beta, Control.parallel);
-    end
-    Fext(BC.fixed) = f_fixed;
+    % Force vectors
+        Fint = K*d;
 
-% Strain
-    [strain, stress] = getStrain(d, Mesh, Material, Control.stress_calc, Quad);   
-
-% Force vectors
-    Fint = K*d;
-
-%% Write to vtk
-    if plot2vtk
-        write2vtk_static(config_name, vtk_dir, Mesh, Control, BC.fixed, d, strain, stress, ...
-                        Fint, Fext);
-    end
-    
-    if progress_on
+    % Write to vtk
+        if plot2vtk
+            write2vtk_quasistatic(config_name, vtk_dir, Mesh, Control, BC.fixed, d, strain, stress, ...
+                            Fint, Fext, step_count);
+        end
+     % Update time variables
+     t = t + dt;
+     step_count = step_count + 1;
+     
+ end
+ 
+     if progress_on
         disp('done')
     end
