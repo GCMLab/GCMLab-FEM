c43971a06e0bc4ac2a87f907f4ee40732e25169a
Bruce <71358608+b3gee@users.noreply.github.com>
2023-07-11 11:55:18 -0400

    Standardize setMaterialModel inputs

--- Functions/Main/setMaterialModel.m
+++ Functions/Main/setMaterialModel.m
@@ -23,65 +23,73 @@ function [Material, stiffnessmatrixfile_name, stressstrainfile_name, Control] = 
         case 'LED1' % Linear elasticity and dynamic
             Material.ConstitutiveLawFile = 'getD';
             Material.StiffnessMatrixFile = 'getK_LE1_dynamic';
-            Material.StressStrainFile = 'getStrain';
             Material.DampingFile = 'getC'; 
+            Material.StressStrainFile = 'getStrain';
             Control.TimeCase = 'dynamic'; 
         case 'LET1' % Linear Elastic Transient
             Material.ConstitutiveLawFile = 'getD';
             Material.StiffnessMatrixFile = 'getK_LET1';
-            Material.StressStrainFile = 'getStrain';
             Material.DampingFile = 'getC'; 
+            Material.StressStrainFile = 'getStrain';
             Control.TimeCase = 'transient'; 
         case 'ST1' % Stiffening Model via 1st Invariant of strain
-            Material.ConstitutiveLawFile = 'getD_ST1';
-            Material.StiffnessMatrixFile = 'getK_ST1';
-            Material.StressStrainFile = 'getStrain_ST1';
-            Material.PostProcessor    = 'write2vtk_eqbm';
-            Material.ProblemType      = 1;
-            Material.Transient        = 0;
+            Material.ConstitutiveLawFile    = 'getD_ST1';
+            Material.StiffnessMatrixFile    = 'getK_ST1';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getStrain_ST1';
+            Material.PostProcessor          = 'write2vtk_eqbm';
+            Material.ProblemType            = 1;
+            Material.Transient              = 0;
         case 'TR1' % Transient Linear Elastic
-            Material.ConstitutiveLawFile = 'getD';
-            Material.StiffnessMatrixFile = 'getK_TR1';
-            Material.StressStrainFile = 'getStrain';
-            Material.PostProcessor    = 'write2vtk_eqbm';
-            Material.ProblemType      = 1;
-            Material.Transient        = 1;
+            Material.ConstitutiveLawFile    = 'getD';
+            Material.StiffnessMatrixFile    = 'getK_TR1';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getStrain';
+            Material.PostProcessor          = 'write2vtk_eqbm';
+            Material.ProblemType            = 1;
+            Material.Transient              = 1;
         case 'TH1' % Thermal Diffusion (Steady-state)
-            Material.ConstitutiveLawFile = 'getD_TH1';
-            Material.StiffnessMatrixFile = 'getK_TH1';
-            Material.StressStrainFile = 'getFlux_TH1';
-            Material.PostProcessor    = 'write2vtk_dfsn';
-            Material.ProblemType      = 2;
-            Material.Transient        = 0;
+            Material.ConstitutiveLawFile    = 'getD_TH1';
+            Material.StiffnessMatrixFile    = 'getK_TH1';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getFlux_TH1';
+            Material.PostProcessor          = 'write2vtk_dfsn';
+            Material.ProblemType            = 2;
+            Material.Transient              = 0;
         case 'TH2' % Thermal Diffusion (Transient)
-            Material.ConstitutiveLawFile = 'getD_TH1';
-            Material.StiffnessMatrixFile = 'getK_TH1';
-            Material.StressStrainFile = 'getFlux_TH1';
-            Material.PostProcessor    = 'write2vtk_dfsn';
-            Material.ProblemType      = 2;
-            Material.Transient        = 1;
+            Material.ConstitutiveLawFile    = 'getD_TH1';
+            Material.StiffnessMatrixFile    = 'getK_TH1';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getFlux_TH1';
+            Material.PostProcessor          = 'write2vtk_dfsn';
+            Material.ProblemType            = 2;
+            Material.Transient              = 1;
             Control.TimeCase = 'static'; 
         case 'ST2' % Softening Model via 1st Invariant of strain
-            Material.ConstitutiveLawFile = 'getD_ST2';
-            Material.StiffnessMatrixFile = 'getK_ST1';
-            Material.StressStrainFile = 'getStrain_ST1';
-			Material.PostProcessor    = 'write2vtk_eqbm';
-            Material.ProblemType      = 1;
-            Material.Transient        = 1;
+            Material.ConstitutiveLawFile    = 'getD_ST2';
+            Material.StiffnessMatrixFile    = 'getK_ST1';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getStrain_ST1';
+			Material.PostProcessor          = 'write2vtk_eqbm';
+            Material.ProblemType            = 1;
+            Material.Transient              = 1;
             Control.TimeCase = 'static'; 
         case 'TR2' % Transient model with stiffening model via 1st invariant of strain
-            Material.ConstitutiveLawFile = 'getD_ST1';
-            Material.StiffnessMatrixFile = 'getK_TR2';
-            Material.StressStrainFile = 'getStrain_ST1';
-			Material.PostProcessor    = 'write2vtk_eqbm';
-            Material.ProblemType      = 1;
-            Material.Transient        = 1;
+            Material.ConstitutiveLawFile    = 'getD_ST1';
+            Material.StiffnessMatrixFile    = 'getK_TR2';
+            Material.DampingFile            = 'getC'; 
+            Material.StressStrainFile       = 'getStrain_ST1';
+			Material.PostProcessor          = 'write2vtk_eqbm';
+            Material.ProblemType            = 1;
+            Material.Transient              = 1;
             Control.TimeCase = 'transient'; 
         case 'VE1' % Linear Viscoelastic Kelvin-Voigt Model
-            Material.ConstitutiveLawFile = 'getD';
-            Material.StiffnessMatrixFile = 'getK_VE1';
-            Material.StressStrainFile = 'getStrain';
-            Material.DampingFile = 'getC_VE1'; 
+            Material.ConstitutiveLawFile    = 'getD';
+            Material.StiffnessMatrixFile    = 'getK_VE1';
+            Material.DampingFile            = 'getC_VE1'; 
+            Material.StressStrainFile       = 'getStrain';
+            Material.ProblemType            = 1;
+            Material.Transient              = 1;
             Control.TimeCase = 'transient'; 
     end
 
