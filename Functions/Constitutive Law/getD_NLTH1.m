aacb2495944a935b76bfeac1ec1e4fdea607f7fd
GZingaro <gzingaro@uwaterloo.ca>
2023-11-14 23:04:30 -0500

    Renamed linear diffusivity coefficient to k, to avoid confusion with the numerical integration parameter alpha.

--- Functions/Constitutive Law/getD_NLTH1.m
+++ Functions/Constitutive Law/getD_NLTH1.m
@@ -1,8 +1,8 @@
-function [D, alpha] = getD_NLTH1(nMat, Material, Mesh, dG)
+function [D, k] = getD_NLTH1(nMat, Material, Mesh, dG)
 %getD_NLTH1 Constitutive law for nonlinear diffusion problem
 %   D = getD_NLTH1(nMat, Material, Mesh, dG) is the nonlinear diffusion 
 %   tensor where the constitutive law is a nonlinear relation given as
-%   k(T) = k0+αT^n. 
+%   D(T) = k0+kT^n. 
 %   --------------------------------------------------------------------
 %   Input
 %   --------------------------------------------------------------------
@@ -23,12 +23,14 @@ function [D, alpha] = getD_NLTH1(nMat, Material, Mesh, dG)
 %                   (a) Linear diffusivity coefficient for isotropic case 
 %                   (b) Linear diffusivity coefficient in x-direction for
 %                       orthotropic case
-%               .k2: 
+%               .a2: 
 %                   (a) Linear diffusivity coefficient in y-direction for
 %                   orthotropic case
 %               .n: Power diffusivity coefficient
 %   dG:         temperature at Gaussian point of element e for current
 %               iteration
+%   D:          nonlinear diffusivity tensor
+%   k:          slope of diffusivity tensor
 %   --------------------------------------------------------------------
 %
 %   Acknowledgements : Jonathan Zingaro
@@ -41,7 +43,7 @@ end
 
 % Define Diffusivity Constant (α and n)
 a1 = Material.Prop(nMat).a1; % For Isotropic and 1D Cases
-if isfield(Material.Prop(nMat),'alpha2')
+if isfield(Material.Prop(nMat),'a2')
     a2 = Material.Prop(nMat).a2; % Orthotropic 2D Case
 end
 
@@ -52,16 +54,16 @@ n = Material.Prop(nMat).n; % n Constant
 switch Mesh.nsd
     case 1
         D = k1+a1*dG.^n;
-        alpha = a1;
+        k = a1;
     case 2
         switch Material.Dtype
             case 'ISO'
-               alpha = a1*eye(2);
-               D = k1*eye(2)+alpha*dG.^n;
+               k = a1*eye(2);
+               D = k1*eye(2)+k*dG.^n;
             case 'ORTHO'
                 k0 = [k1,0;0,k2];
-                alpha = [a1,0;0,a2];
-                D = k0+alpha*dG.^n;
+                k = [a1,0;0,a2];
+                D = k0+k*dG.^n;
         end
 end
 end
