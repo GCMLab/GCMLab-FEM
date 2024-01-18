8e71771df596522baa5e8e6e8ad6a4bb9843e9f9
Bruna <bccampos@uwaterloo.ca>
2024-01-18 13:51:42 -0500

    Add thermoelasticity model to the list of options in master config file

--- Config Files/MasterConfigFile.m
+++ Config Files/MasterConfigFile.m
@@ -132,6 +132,8 @@ function [Mesh, Material, BC, Control] = MasterConfigFile(config_dir, progress_o
         % VE1 - Viscoelaticity with stiffness based damping
         % TH1 - Thermal Diffusion (Steady-State)
         % TH2 - Thermal Diffusion (Transient)
+        % THLE1 - Thermoelasticity (Steady-State)
+        % THLE2 - Thermoelasticity (Transient)
     Material.Model = 'LE1';
     
     % number of material properties
